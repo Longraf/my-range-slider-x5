7401f6ab0ce421da12aba3a56e62241d71d8ff86
Longraf <13longraf@mail.ru>
2019-12-17 21:24:44 +0300

    favicon add

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,15 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 
-var browserSync = require('browser-sync').create(); // live reload
-var sass = require('gulp-sass'); //watch sass
-var plumber = require('gulp-plumber');
-var notify = require("gulp-notify");
-var autoprefixer = require('gulp-autoprefixer'); // в es5 и кросбраузерность
-var sourcemaps = require('gulp-sourcemaps'); // исходные карты
-var del = require('del'); // удаляет всё из нужной папки
-var runSequence = require('run-sequence'); // Зависимости и последовательность выполнения
-let source = require('vinyl-source-stream');
-let browserify = require('browserify');
+const browserSync = require('browser-sync').create(); // live reload
+const sass = require('gulp-sass'); //watch sass
+const plumber = require('gulp-plumber');
+const notify = require("gulp-notify");
+const autoprefixer = require('gulp-autoprefixer'); // в es5 и кросбраузерность
+const sourcemaps = require('gulp-sourcemaps'); // исходные карты
+const del = require('del'); // удаляет всё из нужной папки
+const runSequence = require('run-sequence'); // Зависимости и последовательность выполнения
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
 
 gulp.task('del', function () {
     return del('./build')
@@ -35,8 +35,6 @@ gulp.task('sass', function () {
        .pipe(browserSync.stream())
 });
 
-
-
 gulp.task('html', function () { //copy html in build
     return gulp.src('./src/**/*.html')
         .pipe(plumber())
@@ -44,21 +42,11 @@ gulp.task('html', function () { //copy html in build
         .pipe(browserSync.stream())
 });
 gulp.task('copy:img', function () { //copy img in build/img
-    return gulp.src('src/img/**/*.{img,png,svg,gif}')
+    return gulp.src('src/img/**/*.{img,png,svg,gif,xml,ico}')
         .pipe(gulp.dest('./build/img'))
         .pipe(browserSync.stream())
 });
-// gulp.task('scripts', function () { //copy js in build/js
-//     return gulp.src('src/js/**/*.js')
-//         .pipe(browserify({
-//             entries: './src/js/main.js',
-//             debug: true,
-//         }))
-//
-//         // .pipe(source("main.js"))
-//         .pipe(gulp.dest('./build/js'))
-//
-// });
+
 gulp.task('scripts', function() {
     return browserify({
         entries: './src/js/main.js',
@@ -85,7 +73,7 @@ gulp.task('server', function () {
     gulp.watch('./src/sass/**/*.scss', ['sass']);
     gulp.watch('./src/**/*.html', ['html' ]);
     gulp.watch('./src/js/**/*.js', ['scripts']);
-    gulp.watch('./src/img/**/*.{img,png,svg,gif}', ['copy:img']);
+    gulp.watch('./src/img/**/*.{img,png,svg,gif,xml,ico}', ['copy:img']);
 });
 
 gulp.task('dev', function (callback) {
@@ -103,7 +91,3 @@ gulp.task('dev-not-watch', function (callback) {
         callback
     );
 });
-
-// gulp.task('sass:watch', function () {
-//     gulp.watch('./src/sass/**/*.scss', ['sass'])
-// });
